c0b2452c27569b116813b56d9a7f51a947f2e751
Robert De La Cruz <snailinslowmotion@gmail.com>
2025-07-07 22:13:28 -0400

    fixed courses link in knav

--- src/Kambaz/Navigation.tsx
+++ src/Kambaz/Navigation.tsx
@@ -5,7 +5,10 @@ export default function KambazNavigation() {
       <a href="https://www.northeastern.edu/" id="wd-neu-link" target="_blank">Northeastern</a><br/>
       <Link to="/Kambaz/Account" id="wd-account-link">Account</Link><br/>
       <Link to="/Kambaz/Dashboard" id="wd-dashboard-link">Dashboard</Link><br/>
-      <Link to="/Kambaz/Dashboard" id="wd-course-link">Courses</Link><br/>
+      {/* 
+      TODO: The course link does not work. It's navigating to 
+            dashboard. Confirm that I need to add a courses route in index. */}
+      <Link to="/Kambaz/Courses" id="wd-course-link">Courses</Link><br/> 
       <Link to="/Kambaz/Calendar" id="wd-calendar-link">Calendar</Link><br/>
       <Link to="/Kambaz/Inbox" id="wd-inbox-link">Inbox</Link><br/>
       <Link to="/Labs" id="wd-labs-link">Labs</Link><br/>
--- src/Kambaz/index.tsx
+++ src/Kambaz/index.tsx
@@ -17,6 +17,7 @@ export default function Kambaz() {
               <Route path="/" element={<Navigate to="/Kambaz/Account" />} />
               <Route path="/Account/*" element={<Account />} />
               <Route path="/Dashboard" element={<Dashboard />} />
+              <Route path="/Courses" element={<Courses />} />
               <Route path="/Courses/:cid/*" element={<Courses />} />
               <Route path="/Calendar" element={<h1>Calendar</h1>} />
               <Route path="/Inbox" element={<h1>Inbox</h1>} />
