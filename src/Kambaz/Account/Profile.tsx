44cdbee07ecac16d427ca36e609598b1732e8399
Robert De La Cruz <snailinslowmotion@gmail.com>
2025-07-15 23:49:21 -0400

    A2

--- src/Kambaz/Account/Profile.tsx
+++ src/Kambaz/Account/Profile.tsx
@@ -1,21 +1,45 @@
+import { Form, FormSelect } from "react-bootstrap";
 import { Link } from "react-router-dom";
 
 export default function Profile() {
   return (
     <div id="wd-profile-screen">
       <h3>Profile</h3>
-      <input defaultValue="alice" placeholder="username" className="wd-username"/><br/>
-      <input defaultValue="123"   placeholder="password" type="password"
-             className="wd-password" /><br/>
-      <input defaultValue="Alice" placeholder="First Name" id="wd-firstname" /><br/>
-      <input defaultValue="Wonderland" placeholder="Last Name" id="wd-lastname" /><br/>
-      <input defaultValue="2000-01-01" type="date" id="wd-dob" /><br/>
-      <input defaultValue="alice@wonderland" type="email" id="wd-email" /><br/>
-      <select defaultValue="FACULTY" id="wd-role">
-        <option value="USER">User</option>       <option value="ADMIN">Admin</option>
-        <option value="FACULTY">Faculty</option> <option value="STUDENT">Student</option>
-      </select><br/>
-      <Link to="/Kambaz/Account/Signin" >Sign out</Link>
+      <Form.Control id="wd-username"
+             placeholder="username"
+             className="mb-2"/>
+
+      <Form.Control id="wd-password"
+             placeholder="password" type="password"
+             className="mb-2"/>
+             
+      <Form.Control id="wd-firstname"
+             placeholder="Alice"
+             className="mb-2"/>
+
+      <Form.Control id="wd-lastname"
+             placeholder="Wonderland"
+             className="mb-2"/>
+
+      <Form.Control type="date" id="wd-dob" value="2000-01-21"/>
+
+      <Form.Control id="wd-email"
+             placeholder="alice@wonderland"
+             type="email" 
+             className="mb-2"/>
+
+      <FormSelect>
+        <option selected>FACULTY</option>
+        <option value="USER">User</option>
+        <option value="FACULTY">Faculty</option>
+        <option value="STUDENT">Student</option>
+        <option value="ADMIN">Admin</option>
+      </FormSelect>
+
+      <Link id="wd-signin-btn"
+            to="/Kambaz/Account/Signin"
+            className="btn warning btn-primary w-100 mb-2">
+            Signout </Link>
     </div>
   );
 }
