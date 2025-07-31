8e97e9b5e47cb3c57a53648eddb5fe0299c32966
Robert De La Cruz <snailinslowmotion@gmail.com>
2025-07-31 19:18:37 -0400

    A4

--- src/Kambaz/Courses/Assignments/AssignmentControls.tsx
+++ src/Kambaz/Courses/Assignments/AssignmentControls.tsx
@@ -9,7 +9,7 @@ export default function AssignmentControls() {
       <Button variant="danger" size="lg" className="me-1 float-end" id="wd-add-module-btn">
         <BsPlus className="position-relative me-2" style={{ bottom: "1px" }} />
         Assignment
-      </Button>   
+      </Button>
       <Button variant="secondary" size="lg" className="me-1 float-end" id="wd-add-module-btn">
         <BsPlus className="position-relative me-2" style={{ bottom: "1px" }} />
         Group
